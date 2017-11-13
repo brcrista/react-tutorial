c7fbf488ac088c1c676427c2a0edb3400c1c2a72
Brian Cristante <brcrista@microsoft.com>
2017-11-13 15:58:11 -0500

    Toggle X and O

--- src/index.js
+++ src/index.js
@@ -16,7 +16,8 @@ class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            squares: Array(9).fill(null)
+            squares: Array(9).fill(null),
+            xIsNext: true
         };
     }
 
@@ -29,14 +30,21 @@ class Board extends React.Component {
         );
     }
 
+    nextPlayer() {
+        return this.state.xIsNext ? 'X' : 'O';
+    }
+
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.nextPlayer();
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext
+        });
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = `Next player: ${this.nextPlayer()}`;
 
         return (
             <div>
