9af9b288314bf558624113de084198910286d9ba
Brian Cristante <brcrista@microsoft.com>
2017-11-14 11:12:04 -0500

    Render square as nested function

--- src/index.js
+++ src/index.js
@@ -19,15 +19,6 @@ class Board extends React.Component {
         };
     }
 
-    renderSquare(i) {
-        return (
-            <Square
-                value={this.state.squares[i]}
-                onClick={() => this.handleClick(i)}
-            />
-        );
-    }
-
     nextPlayer() {
         return this.state.xIsNext ? 'X' : 'O';
     }
@@ -57,23 +48,32 @@ class Board extends React.Component {
             }
         })();
 
+        const renderSquare = (i) => {
+            return (
+                <Square
+                    value={this.state.squares[i]}
+                    onClick={() => this.handleClick(i)}
+                />
+            );
+        };
+
         return (
             <div>
                 <div className="status">{status}</div>
                 <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
+                    {renderSquare(0)}
+                    {renderSquare(1)}
+                    {renderSquare(2)}
                 </div>
                 <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
+                    {renderSquare(3)}
+                    {renderSquare(4)}
+                    {renderSquare(5)}
                 </div>
                 <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
+                    {renderSquare(6)}
+                    {renderSquare(7)}
+                    {renderSquare(8)}
                 </div>
             </div>
         );
