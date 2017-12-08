027f2b6976bf740268beee0059da30662b72628f
Brian Cristante <brcrista@microsoft.com>
2017-12-08 09:53:57 -0500

    for..of

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -16,8 +16,7 @@ function winner(squares) {
         [2, 4, 6],
     ];
 
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
+    for (const [a, b, c] of lines) {
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
             return squares[a];
         }
