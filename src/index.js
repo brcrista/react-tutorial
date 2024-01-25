5211fe7a739a50d9d58bed70ec1072d80a4f3729
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2024-01-25 09:26:30 -0500

    Enable strict mode

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Game from "./components/Game";
 import "./index.css";
 
 const root = createRoot(document.getElementById("root"));
+// root.render(<StrictMode><Game /></StrictMode>);
 root.render(<Game />);
